e94c2d31de70c3f490f8a8c9d3f7b910ac3afc91
Danish <itsaws786@gmail.com>
2024-09-21 20:47:00 +0530

    configuration setup of project

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+//we will use storage as a middleware
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
