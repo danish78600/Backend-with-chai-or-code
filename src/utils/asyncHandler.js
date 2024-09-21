5ec7da9050123d3c783a835fbfed3cc84ca088a9
Danish <itsaws786@gmail.com>
2024-09-21 15:21:18 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//wrapper function for promises
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+//wrapper function for async functions
+
+// const asyncHandler = (fn = async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// });
+
+export { asyncHandler };
