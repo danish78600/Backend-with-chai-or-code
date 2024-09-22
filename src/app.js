f32c6255f5f850b8a0ab6d45226cb568e649a89b
Danish <itsaws786@gmail.com>
2024-09-22 06:04:23 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app=express()
+const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/user",userRouter)
 
 
-app.use(express.json({
-    limit:"16kb"  
-}))
-app.use(express.urlencoded())
-app.use(express.static("public"))
-app.use(cookieParser())
+//https://localhost:8000/api/v1/users/register
 
-export{app}
+export { app };
